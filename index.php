cdb0bb692793098fa1ec55d8087cbe261e17a17e
chiaramery <meringolachiara@gmail.com>
2022-11-28 16:14:41 +0100

    Preso valore input con GET

--- index.php
+++ index.php
@@ -39,10 +39,8 @@ var_dump(rnd_number());
 var_dump(rnd_character());
 
 // GET prende il valore che l'user mette nell'input
-$user_lenght = $_GET["lenght-email"];
-if (!empty($user_lenght)) {
-    // $email_num = email_lenght($user_lenght);
-}
+$user_lenght = $_GET["lenght-email"] ?? "";
+var_dump($user_lenght);
 
 ?>
 
@@ -53,18 +51,31 @@ if (!empty($user_lenght)) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
+
     <title>Email Generetor</title>
 </head>
 
 <body>
 
     <main>
-        <form action="index.php" method="GET">
-            <label for="lenght-email">Lunghezza password:</label>
-            <input type="number" id="lenght-email" name="lenght-email">
-            <button type="submit">Invia</button>
-            <button type="reset">Annulla</button>
-        </form>
+
+        <div class="container">
+            <!-- Upper text -->
+            <div class="upper-text">
+                <h1>Strong Password Generetor</h1>
+                <h3>GENERA UNA PASSWORD SICURA</h3>
+            </div>
+            <!-- Form -->
+            <form action="index.php" method="GET">
+                <label for="lenght-email">Lunghezza password:</label>
+                <input type="number" id="lenght-email" name="lenght-email">
+                <button type="submit" class="btn btn-primary">Invia</button>
+                <button type="reset" class="btn btn-secondary">Annulla</button>
+            </form>
+
+        </div>
     </main>
 </body>
 
