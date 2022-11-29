297b21a83b343354a002d4619f4d2a1972f3e1d7
chiaramery <meringolachiara@gmail.com>
2022-11-29 12:32:26 +0100

    Generata password in index.php

--- index.php
+++ index.php
@@ -4,34 +4,40 @@ Una nostra funzione utilizzerà questo dato per generare una password casuale
 Scriviamo tutto (logica e layout) in un unico file *index.php* -->
 
 <?php
-// GET prende il valore che l'user mette nell'input
-$user_lenght = $_GET["lenght-email"] ?? "";
-var_dump($user_lenght);
+// Creo stringhe
+$alphabet = 'abcdefghijklmnopqrstuvwxyz';
+$num = '1234567890';
+$characters = '!?&%$£<>+-()[]@#_="';
 
-// Funzione random lettere minuscole
-function rand_p($user_lenght)
-{
-    $password_element = ["abcdefghijklmnopqrstuvwxyz", "ABCDEFGHIJKLMNOPQRSTUVWXYZ", "1234567890", "!@#$%^&*'£"];
+// Concatenazione stringhe
+$all_string = $alphabet . strtoupper($alphabet) . $num . $characters;
+// var_dump($all_string);
 
+if (isset($_GET["lenght"])) {
+    // GET prende il valore che l'user mette nell'input
+    $user_input = $_GET["lenght"];
     $result = "";
-    if ($user_lenght > 0) {
-        var_dump("é maggiore di 0");
-        for ($i = 0; $i < $user_lenght; $i++) {
-            $rnd_comp = rand(0, count($password_element) - 1);
-            var_dump($rnd_comp);
-            $rnd_element = rand(0, strlen($password_element[$rnd_comp]) - 1);
-            var_dump($rnd_element);
-            $result .= $password_element[$rnd_comp][$rnd_element];
-            var_dump($result);
+
+    // Se l'input dell'utente è compreso tra 8 e 30
+    if ($user_input >= 8 && $user_input <= 30) {
+        $password = "";
+
+        // Mentre la lughezza della stringa è minore del numero inserito dall'utente
+        while (strlen($password) < $user_input) {
+            // Creo un indice random tra 0 e la lunghezza della stringa - 1
+            $ind = rand(0, (strlen($all_string) - 1));
+            // Uso l'indice random per prendere caratteri random dalla stringa completa
+            $sym = $all_string[$ind];
+            // Concateno password con la stringa generata con l'indice randomico
+            $password .= $sym;
         }
-        return $result;
+        // $result prende il valore di $password
+        $result = $password;
     } else {
-        return $result = "Hai sbagliato ad inserire la lunghezza della password";
+        // Sennò da errore
+        $result = "La password deve essere tra 8 e 30 caratteri, cambia preferenza di lunghezza";
     }
 }
-
-
-
 
 ?>
 
@@ -44,36 +50,43 @@ function rand_p($user_lenght)
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Bootstrap -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
-
-    <title>Email Generetor</title>
+    <title>Password Generetor</title>
 </head>
 
-<body>
-
+<body class="bg-info">
     <main>
-
         <div class="container">
             <!-- Upper text -->
-            <div class="upper-text">
+            <div class="up-text col-12">
                 <h1>Strong Password Generetor</h1>
-                <h3>GENERA UNA PASSWORD SICURA</h3>
+                <h4>GENERA UNA PASSWORD SICURA</h4>
             </div>
+
+            <!-- Alert -->
+            <?php if (isset($result)) {
+            ?>
+                <div class="col-5">
+                    <div class="alert alert-info" role="alert">
+                        <!-- Stampo $result nell'alert -->
+                        <?php echo $result; ?>
+                    </div>
+                </div>
+            <?php } ?>
+            <!-- /Alert -->
+
             <!-- Form -->
-            <form action="index.php" method="GET">
-                <label for="lenght-email">Lunghezza password:</label>
-                <input type="number" id="lenght-email" name="lenght-email">
-                <button type="submit" class="btn btn-primary">Invia</button>
-                <button type="reset" class="btn btn-secondary">Annulla</button>
-            </form>
-
-
-            <!-- Risultato -->
-
-            <p> La password generata è:
-                <!-- <?php echo $result; ?> -->
-            </p>
+            <div class="col-5">
+                <form action="index.php">
+                    <label for="lenght">Lunghezza password:</label>
+                    <input type="number" name="lenght" id="lenght" class="form-control">
+                    <button class="btn btn-primary">Invia</button>
+                    <button class="btn btn-secondary">Annulla</button>
+                </form>
+            </div>
+            <!-- /Form -->
         </div>
     </main>
+
 </body>
 
 </html>
